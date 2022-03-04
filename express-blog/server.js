c33c2a697eda3a3c1190576a645d277b4ce8e6b5
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-03-04 14:24:40 +0100

    Update server.js

--- express-blog/server.js
+++ express-blog/server.js
@@ -19,4 +19,6 @@ app.get("/", (req, res) => {
   res.render("index", { posts: posts, admin: req.query.admin || false });
 });
 
-app.listen(3000);
+app.listen(3000, () => 
+  console.log("[server]  App online")
+);
