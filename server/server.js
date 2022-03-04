10d63a0ac73cc0181012ec34ee11b703f90375a9
pintermor9 <86710821+pintermor9@users.noreply.github.com>
2022-03-04 16:29:00 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,16 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var posts = [
+  var posts = get_posts();
+  res.render("index", { posts: posts, admin: req.query.admin || false });
+});
+
+app.listen(process.env.PORT, () => 
+  console.log("[server]  App online")
+);
+
+function get_posts() {
+  return [
     {
       title: "Test 1",
       createdAt: new Date(),
@@ -14,11 +23,14 @@ app.get("/", (req, res) => {
       title: "Test 2",
       createdAt: new Date(),
       description: "description 2",
+    },{
+      title: "Test 3",
+      createdAt: new Date(),
+      description: "descripti3",
+    },{
+      title: "Test 4",
+      createdAt: new Date(),
+      description: "description5",
     },
-  ];
-  res.render("index", { posts: posts, admin: req.query.admin || false });
-});
-
-app.listen(process.env.PORT, () => 
-  console.log("[server]  App online")
-);
+  ]
+} 
